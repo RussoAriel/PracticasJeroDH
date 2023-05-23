15880f7bbe9673f2a4af967f9d178015dcc6efa6
Gustavo Russo <russo.gustavo@gmail.com>
2023-05-23 12:46:34 -0300

    Fin de los ejercicios 9 y 10

--- grusso/ej10.js
+++ grusso/ej10.js
@@ -1,1 +1,26 @@
 /*Al ejercicio 9 agregarle que pueda ingresar los valores de cada alumno por teclado. */
+let readline = require("readline-sync");
+
+let alumnos=[];
+let parcial_1=[];
+let parcial_2=[];
+let parcial_3=[];
+
+for (let index = 0; index < 7; index++) {
+
+  let alumno =readline.question("Ingrese nombre del alumno: ");
+  alumnos.push(alumno);
+  let nota_1 = readline.question("Ingrese la nota del primer parcial: ");
+  parcial_1.push(nota_1);
+  let nota_2 =readline.question("Ingrese la nota del segundo parcial: ");
+  parcial_2.push(nota_2);
+  let nota_3 =readline.question("Ingrese la nota del tercer parcial: ");
+  parcial_3.push(nota_3);
+}
+//console.table(alumnos);
+
+
+console.log("El alumno " + alumnos[6]); 
+console.log("Tiene la nota "+ parcial_1[6] + " en el primer parcial");
+console.log("La nota "+ parcial_2[6] + " en el segundo parcial");
+console.log("La nota "+ parcial_3[6] + " en el tercer parcias");
--- grusso/ej9.js
+++ grusso/ej9.js
@@ -2,3 +2,12 @@
 Guardar nombres de 7 alumnos, su nota del primer parcial, su nota del segundo parcial, su nota del
 tercer parcial. Mostrar toda la información de los 7 alumnos por pantalla.
  */
+let alumnos=["Clara","Carlos","Martin","Santiago","Mariela","Gustavo","Ariel"];
+let parcial_1=[4,5,6,7,8,9,10];
+let parcial_2=[10,9,8,7,6,5,4];
+let parcial_3=[7,6,8,5,9,4,10];
+
+console.log("El alumno " + alumnos[6]); 
+console.log("Tiene la nota "+ parcial_1[6] + " en el primer parcial");
+console.log("La nota "+ parcial_2[6] + " en el segundo parcial");
+console.log("La nota "+ parcial_3[6] + " en el tercer parcias");
