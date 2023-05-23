494c528340851953cce624e8bf1654b66d8861c8
Gustavo Russo <russo.gustavo@gmail.com>
2023-05-23 11:51:18 -0300

    Termino ejercicio 8

--- grusso/ej8.js
+++ grusso/ej8.js
@@ -4,3 +4,8 @@ Definir un arreglo de números. Ubicar en la posición 2 un número y en la posi
 Mostrar los valores de esas posiciones.
 
 */
+let numeros=[];
+numeros[2]=7;
+numeros[8]=16
+console.log(numeros[2]);
+console.log(numeros[8]);
